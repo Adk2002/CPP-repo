3885a53b124a9d62b8d86a3b188b701d36f62a83
Aditya <ankushkarmakar598@gmail.com>
2023-01-30 16:30:21 +0530

    Update the last commit

--- recursion/recursion2.cpp
+++ recursion/recursion2.cpp
@@ -1,18 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int number(int q)       //Resursion function to print 1 to 'n' backwards;
+int number(int q)
 {
-    if(q == 0)
-        return 0;
-    else 
-        cout<<" "<<q;
-        return number(q-1);
+    // Resursion function to print 1 to 'n' backwards;
+    // if (q == 0)
+    //     return 0;
+    // else
+    //     cout << " " << q;
+    // return number(q - 1);
+
+    //Recursion to print number forward;
+    if (q > 0)
+    {
+        number(q - 1);
+        cout << q << " ";
+    }
+    return '\0';
 }
 int main()
 {
     int a;
-    cout<<"Enter any number: ";
-    cin>>a;
-    cout<<number(a);
-return 0;
+    cout << "Enter any number: ";
+    cin >> a;
+    cout << number(a);
+    return 0;
 }
